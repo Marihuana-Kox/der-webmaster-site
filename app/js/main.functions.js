63f5a24646621bdd2279affec7a5340328ad8b24
Marihuana-Kox <yakuhs@yandex.ru>
2023-08-12 13:55:10 +0300

    12.08.23

--- app/js/main.functions.js
+++ app/js/main.functions.js
@@ -17,11 +17,11 @@ $(function() {
     $('a').on('click', function() {
         let ths = $(this) //.attr('href')
         $('li').css({'border-bottom': ''})
-        ths.parent().css({'border-bottom': '1px solid var(--light-darkblue-title)'})
+        ths.parent('li').css({'border-bottom': '1px solid var(--light-darkblue-title)'})
         setTimeout(() => {
             $('.button-menu-block-activ').fadeOut(600);
             $('.list-menu-box').animate({'top': '-60%'}, 600);
             $('.button-menu-block').fadeIn(600);
-        }, 1000)
+        }, 500) 
     });
 })
